a133c7d5382577b92e7a2c34d8303d19a3aa4c88
jablo <jablo@decipher.decipher>
2025-08-03 23:39:57 +0300

    Feat: Server Done and Tested

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -8,19 +8,15 @@ const { protect } = require('../middleware/auth');
 // All routes below are protected
 router.use(protect);
 
-// @route   POST /api/goals
 // @desc    Create a new goal
 router.post('/', createGoal);
 
-// @route   GET /api/goals
 // @desc    Get all goals for logged-in user
 router.get('/', getUserGoals);
 
-// @route   POST /api/goals/:goalId/progress
 // @desc    Add progress to a specific goal and update streak
 router.post('/:goalId/progress', addProgressToGoal);
 
-// @route   DELETE /api/goals/:goalId
 // @desc    Delete a goal
 router.delete('/:goalId', deleteGoal);
 
